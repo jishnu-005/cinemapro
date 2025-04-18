a5e2d88ce6eeb51f58489fe946036dfa8fffb525
jishnu-005 <jishnuarullil2005@gmail.com>
2025-04-18 08:37:59 +0530

    settings.py changed

--- cinemapro/wsgi.py
+++ cinemapro/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cinemapro.settings')
 
 application = get_wsgi_application()
+
+app = application
